32fe95c88ea6d68da82f2cde60aea47d3a7933a0
dashingqi <1415942241@qq.com>
2018-08-15 23:47:42 +0800

    四大线程池的学习

--- threaddemo/src/main/java/com/example/threaddemo/MainActivity.java
+++ threaddemo/src/main/java/com/example/threaddemo/MainActivity.java
@@ -108,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         //单一延迟执行
-        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+//        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
 
 //        scheduledExecutorService.schedule(new Runnable() {
 //            @Override
@@ -120,17 +120,35 @@ public class MainActivity extends AppCompatActivity {
 //        }, 2, TimeUnit.SECONDS);
 
 //      循环有周期的执行  2 代表第一次运行程序 到第一个线程运行的开始间隔  3  代表每个线程运行的间隔。
-        scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+//        scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//                Log.d(TAG,Thread.currentThread().getName());
+//
+//            }
+//        },2,3,TimeUnit.SECONDS);
+//
+//        //停止线程
+//        scheduledExecutorService.shutdown();
+
+        ScheduledExecutorService scheduledExecutorService1 = Executors.newScheduledThreadPool(4);
+        scheduledExecutorService1.scheduleAtFixedRate(new Runnable() {
             @Override
             public void run() {
 
-                Log.d(TAG,Thread.currentThread().getName());
-
+                Log.d(TAG,Thread.currentThread().getName()+"ScheduledThreadPool");
             }
         },2,3,TimeUnit.SECONDS);
 
-        //停止线程
-        scheduledExecutorService.shutdown();
+
+//        scheduledExecutorService1.schedule(new Runnable() {
+//            @Override
+//            public void run() {
+//                Log.d(TAG,Thread.currentThread().getName()+"ScheduledThreadPool");
+//
+//            }
+//        },2,TimeUnit.SECONDS);
 
     }
 }
